4bab2465fba9ec0706c52df996dc475f869d619f
罗展勋 <8442575+luo-zhanxun@user.noreply.gitee.com>
2022-03-03 15:59:42 +0800

    更改我要退出

--- src/components/CommonHeader.vue
+++ src/components/CommonHeader.vue
@@ -14,7 +14,7 @@
         </span>
         <el-dropdown-menu slot="dropdown">
           <el-dropdown-item>个人中心</el-dropdown-item>
-          <el-dropdown-item @click.native="exit">退出登录</el-dropdown-item>
+          <el-dropdown-item @click.native="exit">我要退出</el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
     </div>
